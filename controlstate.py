8ce44441f5d44950c905245df2a1a7370c84228e
Bukkola <86602615+Bukkola@users.noreply.github.com>
2024-11-24 18:49:07 +0100

    Add files via upload

---
+++ controlstate.py
@@ -0,0 +1,32 @@
+# a=15
+# if a > 10:
+#     print("a is greater")
+
+# a= 15
+# b= 20
+
+# if a > b:
+#     print("a is greater")
+
+# else:
+#     print("b is greater")
+
+# a = 15
+# b = 15
+
+# if a > b:
+#     print("a is greater")
+# elif a == b:
+#     print("both are equal")
+# else:
+#     print("b is greater")
+
+a= 14
+b=10
+c = 11
+if (a ==14): 
+    print("The value of a is 15")
+    if ( b ==10 ):
+        print("b is equals to a")
+        if (c == 11):
+            print("Error not in code")
---
+++ loop.py
@@ -0,0 +1,60 @@
+# for x in range (0,3) :
+#     print('Loop execution %d' % (x))
+
+# for letter in 'TutorialsCloud' :
+#     print ('Current letter is:', letter)
+
+    # while loop 
+# count = 1
+
+# while count < 6:
+#     print(count)
+#     count += 1
+
+    # Nested loop
+
+# for g in range (1,6):
+#     for k in range (1,3):
+#         print("%d * %d = %d" % (g, k ,g*k))
+
+
+    #Break Statement
+
+# count = 0
+
+# while count <= 100:
+#     print(count)
+#     count +=1
+#     if count >= 3:
+#         break
+
+
+#continue statement
+
+# for x in range(10):
+#     if x % 2 == 0:
+#         continue 
+#     print (x)
+
+# Pass Statement
+
+# for letter in 'TutorialCloud' :
+#     if letter == 'C' :
+#         pass
+#         print('Pass Block ')
+#     print('Current Letter is: ', letter )
+
+# Break Statement
+count = 0 
+
+# while count <= 100:
+#     print(count)
+#     count += 1
+#     if count == 3:
+#         break
+
+# continue statement
+for i in range (0,5):
+    if i == 3:
+        continue
+    print(i)
